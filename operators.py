ffea8a51385d071593f890490192188eb6a473ef
Alvin390 <alvinkariuki07@gmail.com>
2025-02-04 11:57:23 +0300

    python_continuation

--- operators.py
+++ operators.py
@@ -14,3 +14,16 @@ num3=21
 num4=52
 print(num3==num4) #equal to
 print(num3 !=num4) #not equal to
+print(num3 > num4) #greater than
+print(num3 < num4) #less than
+print(num3 >= num4) #greater than or equal to
+print(num3 <= num4) #less than or equal to
+
+#Logical_operators
+num5=35
+num6=93
+num7=150
+print(num5 >=2 and num6<=32) #and
+print(num5 >=2 or num6<=32) #or
+print(not(num5 >=2 and num6<=32)) #not
+print(not(num5 >=2 or num6<=32)) #not
