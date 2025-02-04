ae1341583a6fd56fdbd977396907b6c126d489b6
Alvin390 <alvinkariuki07@gmail.com>
2025-02-04 11:39:08 +0300

    python_basics

---
+++ operators.py
@@ -0,0 +1,16 @@
+# Arithmetic_operators
+num1=5
+num2=14
+print(num1+num2) #addition
+print(num1-num2) #subtraction
+print(num1*num2) #multiplication
+print(num1/num2) #division
+print(num1%num2) #modulus/remainder
+print(num1**num2) #exponential (17*17)
+print(num1//num2) #floor
+
+#Comparison_operators
+num3=21
+num4=52
+print(num3==num4) #equal to
+print(num3 !=num4) #not equal to
